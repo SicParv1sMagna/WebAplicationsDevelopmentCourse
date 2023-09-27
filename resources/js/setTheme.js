b0198257cc54a7eef0003fa6b196835844a83498
SicParv1sMagna <92353726+SicParv1sMagna@users.noreply.github.com>
2023-09-27 14:23:39 +0300

    deleting unneccessary fields

--- resources/js/setTheme.js
+++
@@ -1,7 +0,0 @@
-const handleChangeTheme = (isChecked) => {
-    if (isChecked.checked) {
-        document.body.setAttribute('light', '');
-    } else {
-        document.body.removeAttribute('light');
-    }
-}
